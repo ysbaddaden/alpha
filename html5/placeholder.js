8c73e1adad610547ba6d2cb6e0fa1335b21c49d8
ysbaddaden <ysbaddaden@gmail.com>
2009-09-17 17:17:34 +0200

    HTML5.Placeholder is now loaded on load, which permits for browser to auto-fill fields (ie login/password) before trying to set the placeholder.

--- html5/placeholder.js
+++ html5/placeholder.js
@@ -1,45 +1,45 @@
 
-(function()
+HTML5.Placeholder = function(element)
 {
-  HTML5.Placeholder = function(element)
-  {
-    /*@cc_on if (element.type == 'password') { return; } @*/
-    
-    this.element = element;
-    this.elementType = this.element.type;
-    
-    this.element.addEventListener('focus', this.removePlaceholder.bind(this), false);
-    this.element.addEventListener('blur',  this.setPlaceholder.bind(this),  false);
-    
-    this.setPlaceholder();
-  }
+  /*@cc_on if (element.type == 'password') { return; } @*/
   
-  HTML5.Placeholder.prototype.removePlaceholder = function()
+  this.element = element;
+  this.elementType = this.element.type;
+  
+  this.element.addEventListener('focus', this.removePlaceholder.bind(this), false);
+  this.element.addEventListener('blur',  this.setPlaceholder.bind(this),  false);
+  
+  this.setPlaceholder();
+}
+
+HTML5.Placeholder.prototype.removePlaceholder = function()
+{
+  if (this.element.value == this.element.getAttribute('placeholder')) {
+    this.element.value = '';
+  }
+  if (this.elementType == 'password') {
+    this.element.type = 'password';
+  }
+  this.element.removeClassName('placeholder');
+}
+
+HTML5.Placeholder.prototype.setPlaceholder = function()
+{
+  if (/^[\s]*$/.test(this.element.value))
   {
-    if (this.element.value == this.element.getAttribute('placeholder')) {
-      this.element.value = '';
-    }
     if (this.elementType == 'password') {
-      this.element.type = 'password';
+      this.element.type = 'text';
     }
-    this.element.removeClassName('placeholder');
+    this.element.addClassName('placeholder');
+    this.element.value = this.element.getAttribute('placeholder');
   }
-  
-  HTML5.Placeholder.prototype.setPlaceholder = function()
-  {
-    if (/^[\s]*$/.test(this.element.value))
-    {
-      if (this.elementType == 'password') {
-        this.element.type = 'text';
-      }
-      this.element.addClassName('placeholder');
-      this.element.value = this.element.getAttribute('placeholder');
-    }
-  }
-  
+}
+
+window.addEventListener('load', function()
+{
   var elements = document.querySelectorAll('[placeholder]');
   Array.prototype.forEach.call(elements, function(element) {
     new HTML5.Placeholder(element);
   });
-})();
+}, false);
 
