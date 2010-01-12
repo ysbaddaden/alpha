25a050243e03d446c8657f04a6598d913b21406d
ysbaddaden <ysbaddaden@gmail.com>
2010-01-12 16:32:25 +0100

    Fixed UI.Overlay with select element in IE6.

--- ui/overlay.js
+++ ui/overlay.js
@@ -2,7 +2,24 @@
 UI.Overlay = function()
 {
   this.element = document.createElement('div');
-  this.element.className = 'overlay';
+  
+  if (!kokone.browser.ie6) {
+    this.element.className = 'overlay';
+  }
+  else
+  {
+    // iframe tricks IE6, which places select inputs over positionned divs :/
+    var iframe = document.createElement('iframe');
+    iframe.src = "javascript:'<html></html>';";
+    iframe.style.cssText += ';position:absolute;border:0;' +
+      'top:0;left:0;width:100%;height:100%;overflow:hidden;filter:alpha(opacity=0);';
+    this.element.appendChild(iframe);
+    
+    var div = document.createElement('div');
+    div.className = 'overlay';
+    div.style.cssText += ';position:absolute;border:0;top:0;left:0;width:100%;height:100%;';
+    this.element.appendChild(div);
+  }
 }
 
 UI.Overlay.prototype.display = function()
@@ -13,14 +30,12 @@ UI.Overlay.prototype.display = function()
   
   var innerWidth  = (window.innerWidth  || document.documentElement.clientWidth);
   var innerHeight = (window.innerHeight || document.documentElement.clientHeight);
-  this.element.setStyle({
-    display: '',
-    position: 'absolute',
-    top:  0,
-    left: 0,
-    width:  ((document.body.clientWidth  > innerWidth)  ? document.body.clientWidth  : innerWidth)  + 'px',
-    height: ((document.body.clientHeight > innerHeight) ? document.body.clientHeight : innerHeight) + 'px'
-  });
+  
+  var width  = ((document.body.clientWidth  > innerWidth)  ? document.body.clientWidth  : innerWidth);
+  var height = ((document.body.clientHeight > innerHeight) ? document.body.clientHeight : innerHeight);
+  
+  this.element.setStyle({display: '', position: 'absolute',
+    top:  0, left: 0, width: width  + 'px', height: height + 'px'});
 }
 
 UI.Overlay.prototype.hide = function() {
