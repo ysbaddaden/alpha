8febd82a7e1e88d952deb9d83115e8cb6ef649d1
ysbaddaden <ysbaddaden@gmail.com>
2010-01-19 12:44:23 +0100

    Build now removes comments in concatenated (but uncompressed) scripts.

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ if ! test -d "build" ; then
   mkdir build
 fi
 
+JSSTRIP="python build/jsstrip.py -fwq"
+YUICOMP="java -jar build/yuicompressor-2.4.2.jar"
+
 cat compat/array.js \
     compat/core.js \
     compat/element/dom.js \
@@ -13,7 +16,7 @@ cat compat/array.js \
     compat/selectors/sly.js \
     compat/xmlhttprequest.js \
     compat/json2.js \
-    > build/compat.js
+    > build/compat.tmp.js
 
 cat addons/object.js \
     addons/array.js \
@@ -24,7 +27,7 @@ cat addons/object.js \
     addons/color.js \
     addons/style.js \
     addons/ajax.js \
-    > build/addons.js
+    > build/addons.tmp.js
 
 cat ui/ui.js \
     ui/overlay.js \
@@ -35,19 +38,32 @@ cat ui/ui.js \
     ui/notification.js \
     ui/autocompleter.js \
     ui/date_picker.js \
-    > build/ui.js
+    > build/ui.tmp.js
 
 cat html5/html5.js \
     html5/placeholder.js \
-    > build/html5.js
+    > build/html5.tmp.js
 
-java -jar build/yuicompressor-2.4.2.jar build/compat.js -o build/compat-compressed.js --type js --charset utf-8
-java -jar build/yuicompressor-2.4.2.jar build/addons.js -o build/addons-compressed.js --type js --charset utf-8
-java -jar build/yuicompressor-2.4.2.jar build/ui.js     -o build/ui-compressed.js     --type js --charset utf-8
-java -jar build/yuicompressor-2.4.2.jar build/html5.js  -o build/html5-compressed.js  --type js --charset utf-8
+echo "Minifying..."
+$JSSTRIP build/compat.tmp.js > build/compat.js
+$JSSTRIP build/addons.tmp.js > build/addons.js
+$JSSTRIP build/ui.tmp.js     > build/ui.js
+$JSSTRIP build/html5.tmp.js  > build/html5.js
 
-gzip -c build/compat-compressed.js > build/compat-compressed.js.gz
-gzip -c build/addons-compressed.js > build/addons-compressed.js.gz
-gzip -c build/ui-compressed.js     > build/ui-compressed.js.gz
-gzip -c build/html5-compressed.js  > build/html5-compressed.js.gz
+echo "Compressing..."
+$YUICOMP build/compat.tmp.js > build/compat-compressed.js
+$YUICOMP build/addons.tmp.js > build/addons-compressed.js
+$YUICOMP build/ui.tmp.js     > build/ui-compressed.js
+$YUICOMP build/html5.tmp.js  > build/html5-compressed.js
+
+#echo "Gzipping..."
+#gzip -c build/compat-compressed.js > build/compat-compressed.js.gz
+#gzip -c build/addons-compressed.js > build/addons-compressed.js.gz
+#gzip -c build/ui-compressed.js     > build/ui-compressed.js.gz
+#gzip -c build/html5-compressed.js  > build/html5-compressed.js.gz
+
+rm build/compat.tmp.js > build/compat.tmp.js
+rm build/addons.tmp.js > build/addons.tmp.js
+rm build/ui.tmp.js     > build/ui.tmp.js
+rm build/html5.tmp.js  > build/html5.tmp.js
 
