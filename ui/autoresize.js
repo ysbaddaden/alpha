67fc33fa453e7da77d1e53d8922156ef0f025bdb
Julien <ysbaddaden@gmail.com>
2010-04-07 14:44:56 +0200

    Rewrote UI.Autoresize, which shall be better now.

--- ui/autoresize.js
+++ ui/autoresize.js
@@ -2,32 +2,29 @@
 UI.Autoresize = function(textarea, callback)
 {
   this.textarea = textarea;
-  this.textarea.addEventListener('keyup', this.check.bind(this), false);
-  this.check();
+  this.textarea.addEventListener('keyup', this.resize.bind(this), false);
 }
 
-UI.Autoresize.prototype.check = function(evt)
+UI.Autoresize.prototype.resize = function()
 {
   if (!this.fake) {
-    this.createFakeTextarea();
+    this.createFake();
   }
-  var text = this.textarea.value.replace(new RegExp("\n", 'g'), '<br/>') + '<br/>';
-  this.fake.innerHTML = text;
-  this.textarea.style.height = this.fake.offsetHeight + 10 + 'px';
+  this.fake.value  = this.textarea.value + "\n";
+  this.textarea.style.height = this.fake.scrollHeight + 'px';
 }
 
-UI.Autoresize.prototype.createFakeTextarea = function(evt)
+UI.Autoresize.prototype.createFake = function(evt)
 {
-  this.fake = document.createElement('div');
+  this.fake = document.createElement('textarea');
   
   var s = this.textarea.currentStyle ? this.textarea.currentStyle :
     ((document.defaultView && document.defaultView.getComputedStyle) ? document.defaultView.getComputedStyle(this.textarea, "") : this.textarea.style);
-
-  this.fake.style.width             = s.width;
-//  this.fake.style.visibility        = 'hidden';
-//  this.fake.style.position          = 'absolute';
-//  this.fake.style.zIndex            = -1;
-  this.fake.style.float            = 'left';
+  
+  this.fake.style.overflow          = 'hidden';
+  this.fake.style.visibility        = 'hidden';
+  this.fake.style.position          = 'absolute';
+  this.fake.style.zIndex            = -1;
   
   this.fake.style.fontFamily        = s.fontFamily;
   this.fake.style.fontWeight        = s.fontWeight;
@@ -44,12 +41,10 @@ UI.Autoresize.prototype.createFakeTextarea = function(evt)
   this.fake.style.borderBottomWidth = s.borderBottomWidth;
   this.fake.style.borderLeftWidth   = s.borderLeftWidth;
   this.fake.style.borderRightWidth  = s.borderRightWidth;
-  this.fake.style.borderTopStyle    = s.borderTopStyle;
-  this.fake.style.borderBottomStyle = s.borderBottomStyle;
-  this.fake.style.borderLeftStyle   = s.borderLeftStyle;
-  this.fake.style.borderRightStyle  = s.borderRightStyle;
   
-  this.textarea.style.overflow  = 'hidden';
+  this.textarea.style.overflowY  = 'hidden';
+  this.textarea.style.resize     = 'none';
   this.textarea.parentNode.insertBefore(this.fake, this.textarea);
+  this.fake.style.width = s.width;
 }
 
